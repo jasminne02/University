6d23572928a4608081cddd2ae055b5b43d152638
jasminne02 <jasmine.yabari02@gmail.com>
2022-05-01 18:33:04 +0300

    New commit

---
+++ OOP (Spring Trimester) 2022/01.05.2022/ConsoleApp5/CountdownClock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public delegate void PrintMessage(string message);
+
+    class CountdownClock
+    {
+        private string message;
+        private int seconds;
+
+        public string Message { get { return message; } }
+        public int Seconds { get { return seconds; } }
+
+        PrintMessage echoTheMessage = Observer.EchoTheMessage;
+
+        public event EventHandler OnPrintMessage;
+        
+        public CountdownClock(string message, int seconds)
+        {
+            this.message = message;
+            this.seconds = seconds;
+
+            Thread.Sleep(seconds * 1000);
+            echoTheMessage(message);
+        }
+
+    }
+}
---
+++ OOP (Spring Trimester) 2022/01.05.2022/ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            string message;
+            int seconds;
+
+            Console.Write("Your message: ");
+            message = Console.ReadLine();
+
+            do
+            {
+                Console.Write("The alarm time in seconds: ");
+                try
+                {
+                    seconds = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input!");
+                    seconds = -1;
+                }
+            } while (seconds < 0);
+
+            CountdownClock alarm = new CountdownClock(message, seconds);
+            
+        }
+    }
+}
