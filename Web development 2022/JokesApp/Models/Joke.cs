b20d1e042116ea4fb99eb2b2e4c6d6e4eac87bea
jasminne02 <jasmine.yabari02@gmail.com>
2022-11-18 14:04:12 +0200

    New commit

---
+++ Web development 2022/JokesApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public int JokeQuestion { get; set; }
+
+        public int JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
