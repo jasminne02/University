811528d15ad7caecc1e586bb5adb0074226fef7b
jasminne02 <jasmine.yabari02@gmail.com>
2022-11-19 20:35:06 +0200

    New commit

---
+++ Web development 2022/WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
