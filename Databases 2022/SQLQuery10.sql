63f361fb52a8678bf61e58023c1f34967440f7d5
jasminne02 <jasmine.yabari02@gmail.com>
2022-11-21 23:39:05 +0200

    New commit

--- Databases 2022/SQLQuery10.sql
+++ Databases 2022/SQLQuery10.sql
@@ -1,27 +1,45 @@
-﻿/* В базата TRADECOMPANY създайте още една таблица под името SHIPPING, 
-в която ще се случва пренасочването към куриерска фирма (SPEEDY или ECONT). 
-Също така ще е нужен външен ключ към вече съществуващата таблица ORDERS (и по конкретно атрибут от нея ORDER_ID).*/
+﻿/*Добавете нов атрибут SHIPPING_ID от тип int, който може да съдържа null стойности, към таблица ORDERS;*/
+ALTER TABLE ORDERS
+ADD SHIPPING_ID INT;
 
+
+
+/*Създайте таблица SHIPPING;*/
 CREATE TABLE SHIPPING
 (
 	SHIPPING_ID INT PRIMARY KEY, 
-	ORDER_ID INT NOT NULL FOREIGN KEY REFERENCES ORDERS,
 	COURIER VARCHAR(10) NOT NULL
 );
 
 
-/*След това въведете данни в таблица SHIPPING за куриерски фирми относно поръчки с идентификатори 2374 и 2359 (по един  запис). */
 
-INSERT INTO SHIPPING (SHIPPING_ID, ORDER_ID, COURIER)
-VALUES (1, 2374, 'SPEEDY');
-INSERT INTO SHIPPING (SHIPPING_ID, ORDER_ID, COURIER)
-VALUES (2, 2359, 'ECONT');
+/*Добавете външен ключ в таблица ORDERS към таблица SHIPPING (свързващ ги по SHIPPING_ID атрибутите);*/
+ALTER TABLE ORDERS
+ADD FOREIGN KEY(SHIPPING_ID) REFERENCES SHIPPING;
 
 
 
-/* Изведете всички данни чрез съединение (join) между новосъздадената таблица SHIPPING и таблица ORDERS.*/
+/*Добавете два записа при новата таблица SHIPPING (с идентификатори 1 и 2 , относно SHIPPING_ID);*/
+INSERT INTO SHIPPING (SHIPPING_ID, COURIER)
+VALUES (1, 'SPEEDY');
+INSERT INTO SHIPPING (SHIPPING_ID, COURIER)
+VALUES (2, 'ECONT');
 
-SELECT S.SHIPPING_ID, S.ORDER_ID, S.COURIER, O.ORDER_DATE, O.CUSTOMER_ID, O.EMPLOYEE_ID, O.SHIP_ADDRESS
+
+
+/*Направете промяна в полетата за SHIPPING_ID при таблица ORDERS, като: поръчка с индентификатор 2374 - 
+придобие стойност 1 в полето при атрибут SHIPPING_ID, а поръчка 2359 - стойност 2.*/
+UPDATE ORDERS
+SET SHIPPING_ID = 1
+WHERE ORDER_ID = 2374;
+
+UPDATE ORDERS
+SET SHIPPING_ID = 2
+WHERE ORDER_ID = 2359;
+
+
+
+/*Изведете всички данни чрез съединение (join) между новосъздадената таблица SHIPPING и таблица ORDERS.*/
+SELECT S.SHIPPING_ID, S.COURIER, O.ORDER_ID, O.ORDER_DATE, O.CUSTOMER_ID, O.EMPLOYEE_ID, O.SHIP_ADDRESS
 FROM SHIPPING AS S
-JOIN ORDERS AS O ON S.ORDER_ID = O.ORDER_ID
-
+JOIN ORDERS AS O ON S.SHIPPING_ID = O.SHIPPING_ID;
